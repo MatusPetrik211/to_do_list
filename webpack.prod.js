0c0814084725b328ed3c4b4450e5798bb91b9d10
MatusPetrik211 <172436057+MatusPetrik211@users.noreply.github.com>
2025-01-01 17:40:11 +0100

    Initial commit

---
+++ webpack.prod.js
@@ -0,0 +1,8 @@
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'eval-source-map',
+});
