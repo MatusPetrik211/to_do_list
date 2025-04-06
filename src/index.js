bf846438f410563a493e1aabc523ab663b50095a
MatusPetrik211 <172436057+MatusPetrik211@users.noreply.github.com>
2025-04-06 17:15:21 +0200

    Implement the fuctionality to create tasks and display them

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
 import "./styles.css"
 
-document.querySelector(".confirm-project").addEventListener("click", displayProjects);
 document.querySelector(".add-project").addEventListener("click", showProjectModal);
+document.querySelector(".confirm-project").addEventListener("click", displayProjects);
+document.querySelector(".add-task").addEventListener("click", showTaskModal);
+document.querySelector(".confirm-task").addEventListener("click", displayTasks);
 
 function createProject() {
     const tasks = [];
-    let name = document.querySelector("#project-name").value;
+    const name = document.querySelector("#project-name").value;
     
     const addTask = function(task) {
         tasks.push(task);
     }
 
-    return {name, addTask};
+    return {name, addTask}
 }
 
-function createTask(title, description, priority, check) {
-    return { title, description, priority, check };
+function createTask() {
+    const name = document.querySelector("#task-name").value;
+    const description = document.querySelector("#task-description").value;
+    const date = document.querySelector("#due-date").value;
+
+    let priority = "None";
+    if (document.querySelector("input[name='priority']:checked")) {
+        priority = document.querySelector("input[name='priority']:checked").value;
+    }
+
+    const finished = false;
+
+    return {name, description, date, priority, finished}
 }
 
 function displayProjects(name) {
@@ -31,12 +44,36 @@ function displayProjects(name) {
     projectDiv.append(projectName);
 
 
-    projectContainer.appendChild(projectDiv);
+    projectContainer.append(projectDiv);
+}
+
+function displayTasks() {
+    const task = createTask();
+
+    const taskContainer = document.querySelector(".task-container");
+    const taskDiv = document.createElement("div");
+    taskDiv.classList.add("task-div");
+
+    const taskName = document.createElement("h1");
+    taskName.classList.add("task-name");
+    taskName.textContent = task.name;
+    taskDiv.append(taskName);
+
+
+    taskContainer.append(taskDiv);
 }
 
 function showProjectModal() {
     const modal = document.querySelector(".project-modal");
     const form = document.querySelector(".project-form");
+
+    form.reset();
+    modal.showModal();
+}
+
+function showTaskModal() {
+    const modal = document.querySelector(".task-modal");
+    const form = document.querySelector(".task-form");
 
     form.reset();
     modal.showModal();
