4e1a5ef6cddd815a3acd92f0107fb46f782c1ed4
MatusPetrik211 <172436057+MatusPetrik211@users.noreply.github.com>
2025-04-22 21:16:10 +0200

    Add the functionality for every project to add tasks separately and to switch off between projects

--- src/index.js
+++ src/index.js
@@ -1,66 +1,80 @@
 import "./styles.css"
 
 document.querySelector(".add-project").addEventListener("click", showProjectModal);
-document.querySelector(".confirm-project").addEventListener("click", displayProjects);
+document.querySelector(".confirm-project").addEventListener("click", createProject);
 document.querySelector(".add-task").addEventListener("click", showTaskModal);
-document.querySelector(".confirm-task").addEventListener("click", displayTasks);
+document.querySelector(".confirm-task").addEventListener("click", () => {
+    if (currentProject) {
+        createTask(currentProject);
+    }
+});
+
+const projects = [];
+let currentProject = null;
 
 function createProject() {
     const tasks = [];
     const name = document.querySelector("#project-name").value;
     
-    const addTask = function(task) {
-        tasks.push(task);
-    }
+    const newProject = { name, tasks};
+    projects.push(newProject);
 
-    return {name, addTask}
+    displayProjects();
 }
 
-function createTask() {
+function createTask(project) {
     const name = document.querySelector("#task-name").value;
     const description = document.querySelector("#task-description").value;
     const date = document.querySelector("#due-date").value;
-
+    const finished = false;
     let priority = "None";
+
     if (document.querySelector("input[name='priority']:checked")) {
         priority = document.querySelector("input[name='priority']:checked").value;
     }
 
-    const finished = false;
+    project.tasks.push({name, description, date, priority, finished});
 
-    return {name, description, date, priority, finished}
+    displayTasks(project);
 }
 
-function displayProjects(name) {
-    const project = createProject();
-
+function displayProjects() {
     const projectContainer = document.querySelector(".project-container");
-    const projectDiv = document.createElement("div");
-    projectDiv.classList.add("project-div");
+    projectContainer.textContent = "";
 
-    const projectName = document.createElement("h1");
-    projectName.classList.add("project-name");
-    projectName.textContent = project.name;
-    projectDiv.append(projectName);
+    for (let project of projects) {
+        const projectDiv = document.createElement("div");
+        projectDiv.classList.add("project-div");
 
+        const projectName = document.createElement("h1");
+        projectName.classList.add("project-name");
+        projectName.textContent = project.name;
+        projectDiv.append(projectName);
 
-    projectContainer.append(projectDiv);
+        projectDiv.addEventListener("click", () => {
+            currentProject = project;
+            displayTasks(project);
+        });
+
+        projectContainer.append(projectDiv);
+    }
 }
 
-function displayTasks() {
-    const task = createTask();
-
+function displayTasks(project) {
     const taskContainer = document.querySelector(".task-container");
-    const taskDiv = document.createElement("div");
-    taskDiv.classList.add("task-div");
+    taskContainer.textContent = "";
 
-    const taskName = document.createElement("h1");
-    taskName.classList.add("task-name");
-    taskName.textContent = task.name;
-    taskDiv.append(taskName);
-
-
-    taskContainer.append(taskDiv);
+    for (let task of project.tasks) {
+        const taskDiv = document.createElement("div");
+        taskDiv.classList.add("task-div");
+    
+        const taskName = document.createElement("h1");
+        taskName.classList.add("task-name");
+        taskName.textContent = task.name;
+        taskDiv.append(taskName);
+    
+        taskContainer.append(taskDiv);
+    }
 }
 
 function showProjectModal() {
@@ -77,4 +91,4 @@ function showTaskModal() {
 
     form.reset();
     modal.showModal();
-}
+} 
