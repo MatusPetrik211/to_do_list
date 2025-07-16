23469dcd18352480c2bdb9a7e4b2cf05849904df
MatusPetrik211 <172436057+MatusPetrik211@users.noreply.github.com>
2025-07-16 13:17:07 +0200

    deployment commit

--- webpack.common.js
+++ webpack.common.js
@@ -1,18 +1,12 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-
 module.exports = {
-    mode: "development",
-    entry: "./src/script.js",
+    entry: "./src/index.js",
     output: {
-      filename: "main.js",
-      path: path.resolve(__dirname, "dist"),
-      clean: true,
-    },
-    devtool: "eval-source-map",
-    devServer: {
-      watchFiles: ["./src/index.html"],
+        filename: "main.js",
+        path: path.resolve(__dirname,"dist"),
+        clean: true,
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -21,18 +15,18 @@ module.exports = {
     ],
     module: {
         rules: [
-          {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-          },
-          {
-            test: /\.html$/i,
-            loader: "html-loader",
-          },
-          {
-            test: /\.(png|svg|jpg|jpeg|gif)$/i,
-            type: "asset/resource",
-          }
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                use: ["html-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            }
         ],
-      },
+    },
 };
